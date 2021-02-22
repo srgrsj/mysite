8f9fcefb9d314feddb4968792512004b2b1d60b4
towar422 <towar4221@gmail.com>
2021-02-22 19:57:52 +0700

    commit4

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Hello World</h1>")
+
+def meme(request):
+    return HttpResponse("<img src='https://memepedia.ru/wp-content/uploads/2021/02/povezlo-povezlo-mem-5.jpg'>")
