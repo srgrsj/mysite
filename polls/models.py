6757f2b54a0cc80eb3a84e470514308fe6b43afa
towar422 <towar4221@gmail.com>
2021-02-27 19:53:33 +0700

    fhksdofhsdufkoas

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    text = models.CharField(max_length=500)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.text
+
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    text = models.CharField(max_length=500)
+    votes = models.IntegerField(default=0)
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question, Choice
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    q_all = Question.objects.all()
+    res = "<ol>"
+    for q in q_all:
+        res += "<li>%s</li>" % q.text
+    res += "</ol>"
 
+    return HttpResponse(res)
+    
 def meme(request):
     return HttpResponse("<img src='https://memepedia.ru/wp-content/uploads/2021/02/povezlo-povezlo-mem-5.jpg'>")
+
+
+def detail(request, q_id):
+    res = "Question number %s. " % q_id
+    return HttpResponse(res)
+def results(request, q_id):
+    res = "Result for question number %s. " % q_id
+    return HttpResponse(res)
+def vote(request, q_id):
+    res = "Vote for question number %s. " % q_id
+    return HttpResponse(res)
