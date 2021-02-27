b6188b49bd20555e889e93ce25cf2f378b6f9018
towar422 <towar4221@gmail.com>
2021-02-27 17:31:37 +0700

    sdf

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'spidran po maincraftu, poehali...o_poVEzLO_PoveZLO'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['towar52.pythonanywhere.com', "127.0.0.1"]
+ALLOWED_HOSTS = ['towar52.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
