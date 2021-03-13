1b6760cbf2227d96403af9fbefab807059607ec7
towar4221 <towar4221@gmail.com>
2021-03-13 19:51:00 +0700

    fsdfs

--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Question, Choice
 
 # Create your views here.
 def index(request):
     question = Question.objects.all()
-    
-    
     context = {
         "questions" : question
 
     }
 
     return render(request, "polls/index.html", context)
-
-
-
-
-
 
 def meme(request):
     
@@ -28,16 +21,32 @@ def meme(request):
 def detail(request, q_id):
     question = Question.objects.get(pk=q_id)
     context = {
-        "question" : question
+        "question" : question,
     }
 
     return render(request, "polls/detail.html",context)
 
 
 
-def results(request, q_id):
-    res = "Result for question number %s. " % q_id
-    return HttpResponse(res)
+
 def vote(request, q_id):
-    res = "Vote for question number %s. " % q_id
-    return HttpResponse(res)
+    choices = request.POST.getlist("choice")
+    question = Question.objects.get(pk=q_id)
+
+    for c_pk in choices:
+        choice = question.choice_set.get(pk=c_pk)
+        choice.votes += 1
+        choice.save()
+
+
+    
+    return HttpResponseRedirect( reverse("polls:results", args=(q_id, )) )
+
+
+def results(request, q_id):
+    question = Question.objects.get(pk=q_id)
+    context = {
+        "question" : question,
+    }
+
+    return render(request, "polls/results.html",context)
